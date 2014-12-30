0c8fa7c1b5e669efea0488d0d1a1898e84e37df7
Shane Madden <shanemadden@fake.email>
2014-12-30 15:40:44 -0700

    Ensure that the serf-version binary is in place before creating a symlink to it

--- manifests/install/download.pp
+++ manifests/install/download.pp
@@ -49,9 +49,10 @@ class serf::install::download (
     }
 
     file { 'serf':
-        path   => "${install_path}/serf",
-        target => "${install_path}/serf-${version}",
-        before => Anchor['serf::install'],
-        notify => Service['serf'],
+        path    => "${install_path}/serf",
+        target  => "${install_path}/serf-${version}",
+        require => File["serf-${version}"],
+        before  => Anchor['serf::install'],
+        notify  => Service['serf'],
     }
 }
