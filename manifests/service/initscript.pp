4aa69444c206442903b71a28b39dcbc1bb058665
Eslam ElHusseiny <eslam.husseiny@gmail.com>
2016-02-02 15:58:40 +0000

    add support for redhat6 init script

--- manifests/service/initscript.pp
+++ manifests/service/initscript.pp
@@ -3,11 +3,15 @@ class serf::service::initscript (
     $install_path = $::serf::install_path,
     $config_dir   = $::serf::config_dir,
 ) {
+  case $::osfamily {
+    'Debian': { $template = 'serf/initscript.erb' }
+    'RedHat': { $template = 'serf/sysvinit.erb' }
+  }
     file { '/etc/init.d/serf':
         owner   => 'root',
         group   => 'root',
         mode    => '0755',
-        content => template('serf/initscript.erb'),
+        content => template($template),
         require => Anchor['serf::install'],
         before  => Anchor['serf::config'],
         notify  => Service['serf'],
